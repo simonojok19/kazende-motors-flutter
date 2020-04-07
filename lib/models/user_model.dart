536bc973663243da01fd8e29be5be9d5defbe922
simonojok19 <simonojok19@gmail.com>
2020-04-07 13:01:51 +0300

    added user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,9 @@
+class User {
+  String email;
+  String id;
+  String password;
+  String created;
+  String providers;
+  String signedIn;
+  User({ this.id, this.email, this.password, this.created, this.providers, this.signedIn});
+}
