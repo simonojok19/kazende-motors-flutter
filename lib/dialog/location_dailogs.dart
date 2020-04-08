4c655ff2100932f252ed3532e2f36dd70b7092a9
simonojok19 <simonojok19@gmail.com>
2020-04-08 22:21:23 +0300

    fully implemented location dialog one

--- lib/dialog/location_dailogs.dart
+++ lib/dialog/location_dailogs.dart
@@ -1,5 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:kazendemotors/models/location_model.dart';
+
 class LocationDialog {
-  static Future<Location> chooseLocation() async {
-    
+  static Future<Location> chooseLocation(BuildContext context) async {
+    return await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Register Location'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Content'),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('CANCEL'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            FlatButton(
+              child: Text('Register', style: TextStyle(color: Colors.green),),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      }
+    );
   }
 }
