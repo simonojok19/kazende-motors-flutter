f3978cd5b0e9b8f27d4d9b31a2d422310166b077
simonojok19 <simonojok19@gmail.com>
2020-04-08 16:25:46 +0300

    added boda boda registration api

---
+++ lib/services/bodaboda_api.dart
@@ -0,0 +1,10 @@
+import 'package:kazendemotors/models/bodboda_model.dart';
+
+abstract class BodaBodaAPI {
+  Stream<List<BodaBoda>> getBodaBodaList();
+  Future<BodaBoda> getBodaBoda(String documentID);
+  Future<bool> registerBodaBoda(BodaBoda bodaBoda);
+  void updateBodaBoda(BodaBoda bodaBoda);
+  void updateBodaBodaWithTransaction(BodaBoda bodaBoda);
+  void deleteBodaBoda(BodaBoda bodaBoda);
+}
