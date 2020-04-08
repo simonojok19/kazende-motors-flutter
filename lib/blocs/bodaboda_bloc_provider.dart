64d4ae0275a69b5336943ff85d3c197d45f8a5be
simonojok19 <simonojok19@gmail.com>
2020-04-08 17:22:13 +0300

    added some codes

---
+++ lib/blocs/bodaboda_bloc_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:kazendemotors/blocs/bodaboda_bloc.dart';
+
+class BodaBodaBlocProvider extends InheritedWidget {
+  final BodaBodaBloc bodaBodaBloc;
+
+  const BodaBodaBlocProvider({ Key key, Widget child, this.bodaBodaBloc}) : super(key: key, child: child);
+
+  static BodaBodaBlocProvider of (BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<BodaBodaBlocProvider>();
+  }
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => false;
+
+}
