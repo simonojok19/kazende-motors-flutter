fec0a2c7bf85d6113b889278fb013bb3a5effcd3
simonojok19 <simonojok19@gmail.com>
2020-04-11 18:09:07 +0300

    added home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,37 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
+      body: GridView.extent(
+        maxCrossAxisExtent: 175.0,
+        scrollDirection: Axis.vertical,
+        padding: EdgeInsets.all(8.0),
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(8.0),
+            child: InkWell(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.motorcycle,
+                    size: 48.0,
+                    color: Colors.blue,
+                  ),
+                  Divider(),
+                  Text(
+                    'BodaBodas',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16.0
+                    ),
+                  ),
+                ],
+              ),
+              onTap: () { },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
