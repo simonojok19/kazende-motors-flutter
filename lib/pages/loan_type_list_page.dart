16a485f09365832ac5e663b44244c2d10eda5814
simonojok19 <simonojok19@gmail.com>
2020-04-13 18:44:38 +0300

    added the loan list stream builder

--- lib/pages/loan_type_list_page.dart
+++ lib/pages/loan_type_list_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:kazendemotors/blocs/authentication_bloc.dart';
 import 'package:kazendemotors/blocs/type_edit/loan_type_edit_bloc.dart';
 import 'package:kazendemotors/blocs/type_edit/loan_type_edit_bloc_provider.dart';
+import 'package:kazendemotors/blocs/type_list/loan_type_list_bloc.dart';
+import 'package:kazendemotors/blocs/type_list/loan_type_list_bloc_provider.dart';
 import 'package:kazendemotors/classes/authentication.dart';
 import 'package:kazendemotors/models/type/loan_type_model.dart';
 import 'package:kazendemotors/pages/loan_type_edit_page.dart';
@@ -12,10 +15,43 @@ class LoanTypeListPage extends StatefulWidget {
 }
 
 class _LoanTypeListPageState extends State<LoanTypeListPage> {
+  LoanTypeListBloc _loanTypeListBloc;
+  AuthenticationBloc _authenticationBloc;
+
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _loanTypeListBloc = LoanTypeListBlocProvider.of(context).loanTypeListBloc;
+  }
+
+
+  @override
+  void dispose() {
+    _loanTypeListBloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Loan Types'),),
+      body: StreamBuilder(
+        stream: _loanTypeListBloc.loanTypeList,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if ( snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasData) {
+            return _buildListViewSeparated(snapshot);
+          } else {
+            return Center(
+              child: Text('Add Loan Types'),
+            )
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(
