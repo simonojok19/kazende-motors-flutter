d0ed55e3eaa9255a1416b8c6420f32c4108612df
simonojok19 <simonojok19@gmail.com>
2020-04-13 16:21:36 +0300

    loan type bloc provider complete

---
+++ lib/blocs/type/loan_type_bloc_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:kazendemotors/classes/authentication_api.dart';
+import 'package:kazendemotors/services/loan_type/loan_type_api.dart';
+
+class LoanTypeBlocProvider extends InheritedWidget {
+  final AuthenticationApi authenticationApi;
+  final LoanTypeApi loanTypeApi;
+
+  const LoanTypeBlocProvider({ Key key, Widget child, this.authenticationApi, this.loanTypeApi}): super(key: key, child: child);
+
+  static LocationBlocProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<LoanTypeBlocProvider>();
+  }
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => false;
+  }
+
+}
+
