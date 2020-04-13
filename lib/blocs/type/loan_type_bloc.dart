e77ac209041ebbc30b58a1244a355c275b7dfc90
simonojok19 <simonojok19@gmail.com>
2020-04-13 15:59:03 +0300

    added all controlllers

--- lib/blocs/type/loan_type_bloc.dart
+++ lib/blocs/type/loan_type_bloc.dart
@@ -1,3 +1,36 @@
-class LoanTypeBloc {
+import 'dart:async';
 
+import 'package:kazendemotors/classes/authentication_api.dart';
+import 'package:kazendemotors/models/type/loan_type_model.dart';
+import 'package:kazendemotors/services/loan_type/loan_type_api.dart';
+
+class LoanTypeBloc {
+  final LoanTypeApi loanTypeApi;
+  final AuthenticationApi authenticationApi;
+  final bool add;
+  LoanType loanType;
+
+  StreamController<String> _nameStreamController = StreamController<String>.broadcast();
+  Sink<String> get nameChanged => _nameStreamController.sink;
+  Stream<String> get name => _nameStreamController.stream;
+
+  StreamController<String> _paymentStreamController =
+  StreamController<String>.broadcast();
+  Sink<String> get paymentChanged => _paymentStreamController.sink;
+  Stream<String> get payment => _paymentStreamController.stream;
+
+  StreamController<String> _itemStreamController =
+  StreamController<String>.broadcast();
+  Sink<String> get itemChanged => _itemStreamController.sink;
+  Stream<String> get item => _itemStreamController.stream;
+
+  StreamController<String> _quantityStreamController =
+  StreamController<String>.broadcast();
+  Sink<String> get quantityChanged => _quantityStreamController.sink;
+  Stream<String> get quantity => _quantityStreamController.stream;
+
+  StreamController<String> _descriptionStreamController =
+  StreamController<String>.broadcast();
+  Sink<String> get descriptionChanged => _descriptionStreamController.sink;
+  Stream<String> get description => _descriptionStreamController.stream;
 }
