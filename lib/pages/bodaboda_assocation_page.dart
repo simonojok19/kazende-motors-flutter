e0355f58040bdd3880233cc1b1548655b45c5075
simonojok19 <simonojok19@gmail.com>
2020-04-14 08:08:19 +0300

    added the association page

---
+++ lib/pages/bodaboda_assocation_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BodabodaAssociationPage extends StatefulWidget {
+  @override
+  _BodabodaAssociationPageState createState() => _BodabodaAssociationPageState();
+}
+
+class _BodabodaAssociationPageState extends State<BodabodaAssociationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Association'),),
+    );
+  }
+}
