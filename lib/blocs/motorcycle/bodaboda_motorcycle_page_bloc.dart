34b0925338dd320f5286414b138cf5d982063dc8
simonojok19 <simonojok19@gmail.com>
2020-04-15 14:35:51 +0300

    added motor cycle bloc

---
+++ lib/blocs/motorcycle/bodaboda_motorcycle_page_bloc.dart
@@ -0,0 +1,3 @@
+class BodaBodaMotorCyclePageBloc {
+  const BodaBodaMotorCyclePageBloc();
+}
---
+++ lib/pages/bodaboda_motorcycle_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BodaBodaMotorCyclePage extends StatefulWidget {
+  @override
+  _BodaBodaMotorCyclePageState createState() => _BodaBodaMotorCyclePageState();
+}
+
+class _BodaBodaMotorCyclePageState extends State<BodaBodaMotorCyclePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
