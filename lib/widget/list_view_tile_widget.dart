32e535aa483749576be355a4a7ace7e479e805a6
simonojok19 <simonojok19@gmail.com>
2020-04-19 15:12:36 +0300

    added some codes

--- lib/widget/list_view_tile_widget.dart
+++ lib/widget/list_view_tile_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,6 +13,37 @@ class ListViewTileWidget extends StatefulWidget {
 class _ListViewTileWidgetState extends State<ListViewTileWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    AsyncSnapshot _snapshot = widget.snapshot;
+    return ListView.separated(
+      itemCount: _snapshot.data.length,
+      itemBuilder: (BuildContext context, int index) => ListTile(
+        leading: Column(
+          children: <Widget>[
+            Text(
+              '20th',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 32.0,
+                color: Colors.blue
+              ),
+            ),
+            Text(
+              '2019'
+            )
+          ],
+        ),
+        title: Text(
+          '20, 000',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          'Spear Parts'
+        ),
+        trailing: Icon(Icons.close),
+      ),
+      separatorBuilder: (BuildContext context, int index) => Container(),
+    );
   }
 }
