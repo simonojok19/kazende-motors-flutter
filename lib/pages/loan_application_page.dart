fd136683b4123db53a05240940b5e924000872dc
simonojok19 <simonojok19@gmail.com>
2020-04-19 15:55:40 +0300

    added another page

---
+++ lib/pages/loan_application_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoanApplicationPage extends StatefulWidget {
+  @override
+  _LoanApplicationPageState createState() => _LoanApplicationPageState();
+}
+
+class _LoanApplicationPageState extends State<LoanApplicationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/loan_list_page.dart
+++ lib/pages/loan_list_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:kazendemotors/blocs/loan_application/loan_application_bloc.dart';
+import 'package:kazendemotors/blocs/loan_application/loan_application_bloc_provider.dart';
 import 'package:kazendemotors/blocs/loan_list/loan_list.bloc_provider.dart';
 import 'package:kazendemotors/blocs/loan_list/loan_list_bloc.dart';
+import 'package:kazendemotors/services/loan/loan_service.dart';
 import 'package:kazendemotors/widget/list_view_tile_widget.dart';
 
 class LoanListPage extends StatefulWidget {
@@ -41,6 +44,16 @@ class _LoanListPageState extends State<LoanListPage> {
           } else return Center(
             child: Text('No Loan History to Show'),
           );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.edit),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(
+            builder: (BuildContext context) => LoanApplicationBlocProvider(
+              child: ,
+            )
+          ));
         },
       ),
     );
