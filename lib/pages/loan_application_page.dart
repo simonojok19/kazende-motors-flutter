bdd1a962aed92bbe86171648f62457cd6d29dd61
simonojok19 <simonojok19@gmail.com>
2020-04-19 16:26:45 +0300

    testing the dispose

--- lib/pages/loan_application_page.dart
+++ lib/pages/loan_application_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kazendemotors/blocs/loan_application/loan_application_bloc.dart';
+import 'package:kazendemotors/blocs/loan_application/loan_application_bloc_provider.dart';
 
 class LoanApplicationPage extends StatefulWidget {
   @override
@@ -6,10 +8,35 @@ class LoanApplicationPage extends StatefulWidget {
 }
 
 class _LoanApplicationPageState extends State<LoanApplicationPage> {
+  LoanApplicationBloc _loanApplicationBloc;
+
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _loanApplicationBloc = LoanApplicationBlocProvider.of(context).loanApplicationBloc;
+  }
+
+
+  @override
+  void dispose() {
+    _loanApplicationBloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Apply For Loan'),),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
